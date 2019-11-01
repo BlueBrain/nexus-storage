eba4a281c35091688eb3173146ec75e8fc55bbe9
Didac Montero <didac.monteromendez@epfl.ch>
2019-11-01 18:04:01 +0100

    Added necessary folder permissions to execute move

--- permissions-fixer/src/config.rs
+++ permissions-fixer/src/config.rs
@@ -18,6 +18,7 @@ const GID: Option<&'static str> = option_env!("NEXUS_GROUP_ID");
 const GID_DEFAULT: gid_t = 1000;
 
 pub const FILE_MASK: u32 = 0o440;
+pub const CHMOD_MASK_WX_GROUP: u32 = 0b000011000;
 pub const DIR_MASK: u32 = 0o750;
 
 pub fn get_path_prefix() -> &'static str {
--- permissions-fixer/src/path.rs
+++ permissions-fixer/src/path.rs
@@ -9,6 +9,8 @@ use walkdir::WalkDir;
 use crate::config::*;
 use crate::errors::Failure;
 use crate::errors::Failure::*;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 pub fn apply_permissions(path: &str) -> Result<(), Failure> {
     check_path(path).and_then(check_links).and_then(visit_all)
@@ -47,7 +49,13 @@ fn check_links(path: &Path) -> Result<&Path, Failure> {
     })
 }
 
+fn add_parent_permissions(parent: &Path) -> Result<(), Failure> {
+    let mask = fetch_permissions(parent) | CHMOD_MASK_WX_GROUP;
+    set_permissions(parent, mask)
+}
+
 fn visit_all(path: &Path) -> Result<(), Failure> {
+    let parent_perms = path.parent().map_or(Ok(()), |parent| add_parent_permissions(parent));
     let result: Result<Vec<()>, Failure> = WalkDir::new(path)
         .into_iter()
         .map(|e| {
@@ -60,7 +68,8 @@ fn visit_all(path: &Path) -> Result<(), Failure> {
             }
         })
         .collect();
-    result.map(|_| ())
+    parent_perms.and_then(|_| result.map(|_| ()))
+
 }
 
 fn set_owner(path: &Path) -> Result<(), Failure> {
@@ -85,11 +94,15 @@ fn set_permissions(path: &Path, mask: u32) -> Result<(), Failure> {
     }
 }
 
+fn fetch_permissions(path: &Path) -> u32 {
+    let _metadata = fs::metadata(path).expect("failed to read file metadata");
+    _metadata.permissions().mode()
+}
+
 #[cfg(test)]
 mod tests {
-    use std::fs;
     use std::io;
-    use std::os::unix::fs::{symlink, MetadataExt, PermissionsExt};
+    use std::os::unix::fs::{symlink, MetadataExt};
 
     use rand::{thread_rng, Rng};
 
@@ -171,6 +184,7 @@ mod tests {
         assert!(touch(file_a).is_ok());
         assert!(touch(file_b).is_ok());
         assert!(touch(file_c).is_ok());
+        check_permissions(Path::new("/tmp/nexus-fixer/"), 0o755);
         assert!(visit_all(Path::new("/tmp/nexus-fixer/a")).is_ok());
 
         let uid = get_uid().expect("failed to read UID");
@@ -183,6 +197,8 @@ mod tests {
         check_owner(Path::new("/tmp/nexus-fixer/a/b"), uid, gid);
         check_permissions(Path::new("/tmp/nexus-fixer/a/b/c"), DIR_MASK);
         check_owner(Path::new("/tmp/nexus-fixer/a/b/c"), uid, gid);
+        check_permissions(Path::new("/tmp/nexus-fixer/"), 0o775);
+
         // files
         check_permissions(file_a, FILE_MASK);
         check_owner(file_a, uid, gid);
@@ -200,8 +216,7 @@ mod tests {
     }
 
     fn check_permissions(path: &Path, mask: u32) {
-        let metadata = fs::metadata(path).expect("failed to read file metadata");
-        assert_eq!(metadata.permissions().mode() & 0o777, mask);
+        assert_eq!(fetch_permissions(path) & 0o777, mask);
     }
 
     fn random_mask() -> u32 {
